c42a14a9df2734991aeda14a27417926f53dc740
sdhan <sdhan@suresofttech.com>
2023-06-15 09:26:02 +0900

    gRPC-FIT scenario2 complete

--- src/main/java/com/example/grpc/tree/FindMethod.java
+++
@@ -1,9 +0,0 @@
-package com.example.grpc.tree;
-
-import java.util.List;
-
-public interface FindMethod {
-    List<Integer> findInOrder();
-    List<Integer> findPreOrder();
-    List<Integer> findPostOrder();
-}
